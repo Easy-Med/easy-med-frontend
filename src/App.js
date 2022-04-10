c50fdc66757b494260ba71fcf20436c56be98488
Michał <michalkoks99@gmail.com>
2022-04-10 23:43:12 +0200

    Added patient info

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./easymed/Home";
+import Patient from './easymed/Patient';
 
 function App() {
-  return (
-      <Router>
-          <Routes>
-              <Route path={"/"} element={<Home />} />
-          </Routes>
-      </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path={"/"} element={<Home />} />
+                <Route path={"/patient"} element={<Patient />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/easymed/Patient.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Box, TextField, Typography } from "@mui/material";
+
+const Patient = () => {
+      return (
+            <Box sx={{ width: '100%', mt: 5, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                  <Typography variant={"h1"}>Here will be our App! 🩺</Typography>
+                  <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: "column" }}>
+                        <Typography variant="h5">Patient's information</Typography>
+                        <TextField
+                              label="Name"
+                              defaultValue="Michal"
+                              InputProps={{
+                                    readOnly: true,
+                              }}
+                              margin="normal"
+                        />
+                        <TextField
+                              label="Surname"
+                              defaultValue="Michal"
+                              InputProps={{
+                                    readOnly: true,
+                              }}
+                              margin="normal"
+                        />
+                        <TextField
+                              label="Age"
+                              defaultValue="22"
+                              InputProps={{
+                                    readOnly: true,
+                              }}
+                              margin="normal"
+                        />
+                        <TextField
+                              label="Date of birth"
+                              defaultValue="01-01-2000"
+                              InputProps={{
+                                    readOnly: true,
+                              }}
+                              margin="normal"
+                        />
+                  </Box>
+
+            </Box>
+      );
+};
+
+export default Patient;
