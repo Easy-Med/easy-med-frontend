957fa549ebf1738b4c35e023a3b9e15f82a63bf4
Adrian <a.milewski@onet.eu>
2022-04-13 11:43:43 +0200

    First version of MenuBar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./easymed/Home";
 import ChooseLoginOption from "./app/auth/login/ChooseLoginOption";
 import Register from "./app/auth/register/Register";
 import LoginAsDoctor from "./app/auth/login/LoginAsDoctor";
 import LoginAsPatient from "./app/auth/login/LoginAsPatient";
+import Dashboard from "./easymed/Dashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path={"/"} element={<Home />} />
+        <Route path={"/"} element={<Dashboard />} />
         <Route path={"/login"} element={<ChooseLoginOption />} />
         <Route path={"/login/doctor"} element={<LoginAsDoctor />} />
         <Route path={"/login/patient"} element={<LoginAsPatient />} />
