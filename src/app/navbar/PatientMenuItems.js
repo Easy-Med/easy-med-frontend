b5b70608007b7bdf2ce8273d92ce813a5f9c5779
Adrian <a.milewski@onet.eu>
2022-04-15 16:08:33 +0200

    Add reserve visit button

--- src/app/navbar/PatientMenuItems.js
+++ src/app/navbar/PatientMenuItems.js
@@ -4,6 +4,7 @@ import EventSeatIcon from "@mui/icons-material/EventSeat";
 import MedicationIcon from "@mui/icons-material/Medication";
 import RateReviewIcon from "@mui/icons-material/RateReview";
 import SettingsIcon from "@mui/icons-material/Settings";
+import {Box, Button, IconButton} from "@mui/material";
 
 const mainSideMenuOptions = [
   {
@@ -33,11 +34,24 @@ const secondarySideMenuOptions = [
 
 const PatientMenuItems = ({ open, ...props }) => {
   return (
-    <MenuItems
-      open={open}
-      role={"patient"}
-      itemsArray={[mainSideMenuOptions, secondarySideMenuOptions]}
-    />
+    <Box>
+      <Box
+        sx={{ width: "100%", mt: 1, display: "flex", justifyContent: "center" }}
+      >
+        {open ? (
+          <Button variant={"contained"}>Reserve visit</Button>
+        ) : (
+          <IconButton size={"medium"} color={"primary"}>
+            <EventSeatIcon />
+          </IconButton>
+        )}
+      </Box>
+      <MenuItems
+        open={open}
+        role={"patient"}
+        itemsArray={[mainSideMenuOptions, secondarySideMenuOptions]}
+      />
+    </Box>
   );
 };
 
