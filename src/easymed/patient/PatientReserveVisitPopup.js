ebd52c3a3b0eeeb5ab6899dafef8444ced6348f8
malinowa <64161347+malinowa@users.noreply.github.com>
2022-04-19 23:22:32 +0200

    Changed doctor and specialization fields to autocomplete, code refactoring, created SelectOptionComponent

--- src/easymed/patient/PatientReserveVisitPopup.js
+++ src/easymed/patient/PatientReserveVisitPopup.js
@@ -1,146 +1,159 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import { Box, MenuItem, TextField } from '@mui/material';
-import SelectDateField from './reserve-visit-components/SelectDateField';
-import ReserveVisitTitle from './reserve-visit-components/ReserveVisitTitle';
-import ChooseReserveVisitOption from './reserve-visit-components/ChooseReserveVisitOption';
+import * as React from "react";
+import { Box, Dialog, Button } from "@mui/material";
+import SelectOptionField from "./reserve-visit-components/SelectOptionField";
+import ReserveVisitTitle from "./reserve-visit-components/ReserveVisitTitle";
+import ChooseReserveVisitOption from "./reserve-visit-components/ChooseReserveVisitOption";
 
-const specializationList = ['Orthopedic', 'Kardiologist','Radiologist'] 
-const doctorsList = ['Maciej Bulwa', 'Adrian Kunsz','Barbara Telejko'] 
+const specializationsList = ["Orthopedic", "Kardiologist", "Radiologist"];
+const doctorsList = ["Maciej Bulwa", "Adrian Kunsz", "Barbara Telejko"];
 
-function PatientReserveVisitPopup({ openDialog, setOpenDialog, setShowSuccessAlert,  ...props }) {
-    const [selectedDate, setSelectedDate] = React.useState(null);
-    const [reservationOption, setReservationOption] = React.useState("default");
-    const [selectedSpecialization, setSelectedSpecialization] = React.useState('');
-    const [selectedDoctor, setSelectedDoctor] = React.useState("");
-    const isAllDataComplete = (selectedDate && selectedSpecialization !== '' && selectedDoctor !== '');
+function PatientReserveVisitPopup({
+  openDialog,
+  setOpenDialog,
+  setShowSuccessAlert,
+  ...props
+}) {
+  const [selectedDate, setSelectedDate] = React.useState(null);
+  const [reservationOption, setReservationOption] = React.useState("default");
+  const [selectedSpecialization, setSelectedSpecialization] =
+    React.useState(null);
+  const [selectedDoctor, setSelectedDoctor] = React.useState(null);
+  const isAllDataComplete =
+    selectedDate && selectedSpecialization && selectedDoctor;
 
-    const handleClose = () => {
-      setOpenDialog(false); 
-      setTimeout(() => {
-        setReservationOption("default"); 
-        setSelectedDate(null); 
-        setSelectedSpecialization('');
-        setSelectedDoctor('');}
-        , 400);   
-    };
+  const handleClose = () => {
+    setOpenDialog(false);
+    setTimeout(() => {
+      setReservationOption("default");
+      setSelectedDate(null);
+      setSelectedSpecialization(null);
+      setSelectedDoctor(null);
+    }, 400);
+  };
 
-    const handleSubmitVisit = () => {
-      setShowSuccessAlert(true);
-      console.log("HELOOOOOO");
-      handleClose();
-    }
-  
-    return (
-      <>
-          <Dialog open={openDialog} onClose={handleClose} fullWidth maxWidth="md">
+  const handleSubmitVisit = () => {
+    setShowSuccessAlert(true);
+    handleClose();
+  };
+
+  return (
+    <>
+      <Dialog open={openDialog} onClose={handleClose} fullWidth maxWidth="md">
+        <Box
+          sx={{
+            width: "100%",
+            height: "100%",
+            mt: 2,
+            mb: 2,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <ReserveVisitTitle />
+          {reservationOption === "default" ? (
+            <ChooseReserveVisitOption
+              setReservationOption={setReservationOption}
+            />
+          ) : reservationOption === "date" ? (
             <Box
-            sx={{
-              width: "100%",
-              height: "100%",
-              mt: 2,
-              mb: 2,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
+              sx={{
+                width: "80%",
+                mt: 4,
+                mb: 4,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                gap: 5,
               }}
             >
-              <ReserveVisitTitle/>
-            {reservationOption === "default"   
-              ? 
-              <ChooseReserveVisitOption setReservationOption={setReservationOption}/>
-                  :
-                  (reservationOption === "date" ? 
-                  <Box
-                  sx={{
-                    width: "80%",
-                    mt: 4,
-                    mb: 4,
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    gap: 5,
-                  }}
+              <SelectOptionField
+                type="date"
+                value={selectedDate}
+                valueChanger={setSelectedDate}
+              />
+              {selectedDate ? (
+                <SelectOptionField
+                  type="specialization"
+                  label="Select specialization"
+                  value={selectedSpecialization}
+                  valueChanger={setSelectedSpecialization}
+                  options={specializationsList}
+                />
+              ) : null}
+
+              {selectedDate && selectedSpecialization ? (
+                <SelectOptionField
+                  type="doctor"
+                  label="Select doctor"
+                  value={selectedDoctor}
+                  valueChanger={setSelectedDoctor}
+                  options={doctorsList}
+                />
+              ) : null}
+
+              {isAllDataComplete ? (
+                <Button
+                  variant="contained"
+                  color={"primary"}
+                  onClick={handleSubmitVisit}
                 >
-                  <SelectDateField currentDate={selectedDate} onChange={setSelectedDate}/>
-                  {selectedDate
-                        ? 
-                        <TextField label="Select specialization" 
-                          value={selectedSpecialization} 
-                          onChange={(event) => {setSelectedSpecialization(event.target.value)}} 
-                          select
-                          fullWidth
-                          >
-                     {specializationList.map((spec, idx) => <MenuItem key={idx} value={`S${idx}`}>{spec}</MenuItem>)}
-                      </TextField> 
-                        : 
-                        null}
+                  CONFIRM VISIT
+                </Button>
+              ) : null}
+            </Box>
+          ) : (
+            <Box
+              sx={{
+                width: "80%",
+                mt: 4,
+                mb: 4,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                gap: 5,
+              }}
+            >
+              <SelectOptionField
+                type="specialization"
+                label="Select specialization"
+                value={selectedSpecialization}
+                valueChanger={setSelectedSpecialization}
+                options={specializationsList}
+              />
+              {selectedSpecialization !== "" ? (
+                <SelectOptionField
+                  type="doctor"
+                  label="Select doctor"
+                  value={selectedDoctor}
+                  valueChanger={setSelectedDoctor}
+                  options={doctorsList}
+                />
+              ) : null}
 
-                  {(selectedDate && selectedSpecialization !== '')
-                    ?
-                    <TextField label="Select doctor" 
-                          value={selectedDoctor} 
-                          onChange={(event) => {setSelectedDoctor(event.target.value)}} 
-                          select
-                          fullWidth
-                          >
-                     {doctorsList.map((doc, idx) => <MenuItem key={idx} value={`S${idx}`}>{doc}</MenuItem>)}
-                  </TextField> 
-                    :
-                    null
-                  }
+              {selectedDoctor !== "" && selectedSpecialization !== "" ? (
+                <SelectOptionField
+                  type="date"
+                  value={selectedDate}
+                  valueChanger={setSelectedDate}
+                />
+              ) : null}
 
-                  {isAllDataComplete ? <Button variant="contained" color={"primary"} onClick={handleSubmitVisit}>CONFIRM VISIT</Button> : null}
-                  
-                </Box> 
-                : <Box
-                    sx={{
-                      width: "80%",
-                      mt: 4,
-                      mb: 4,
-                      display: "flex",
-                      flexDirection: "column",
-                      alignItems: "center",
-                      gap: 5,
-                    }}>
-                
-                <TextField label="Select specialization" 
-                        value={selectedSpecialization} 
-                        onChange={(event) => {setSelectedSpecialization(event.target.value)}} 
-                        select
-                        fullWidth
-                        >
-                   {specializationList.map((spec, idx) => <MenuItem key={idx} value={`S${idx}`}>{spec}</MenuItem>)}
-                </TextField> 
-                {selectedSpecialization !== ''
-                      ? 
-                      <TextField label="Select doctor" 
-                        value={selectedDoctor} 
-                        onChange={(event) => {setSelectedDoctor(event.target.value)}} 
-                        select
-                        fullWidth
-                        >
-                   {doctorsList.map((doc, idx) => <MenuItem key={idx} value={`S${idx}`}>{doc}</MenuItem>)}
-                </TextField> 
-                      : 
-                      null}
-
-                {(selectedDoctor !== '' && selectedSpecialization !== '')
-                  ?
-                  <SelectDateField currentDate={selectedDate} onChange={setSelectedDate}/>
-                  :
-                  null
-                }
-
-                {isAllDataComplete ? <Button variant="contained" color={"primary"} onClick={handleSubmitVisit}>CONFIRM VISIT</Button> : null}
-                
-              </Box>
-                )}
-          </Box>
-          </Dialog>
-      </>
-    );
-  }
+              {isAllDataComplete ? (
+                <Button
+                  variant="contained"
+                  color={"primary"}
+                  onClick={handleSubmitVisit}
+                >
+                  CONFIRM VISIT
+                </Button>
+              ) : null}
+            </Box>
+          )}
+        </Box>
+      </Dialog>
+    </>
+  );
+}
 
 export default PatientReserveVisitPopup;
