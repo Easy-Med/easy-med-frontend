ad92ccbb4a27a496fdedd875141db296ceaf134f
malinowa <64161347+malinowa@users.noreply.github.com>
2022-04-19 17:58:43 +0200

    Added form for doctor option, input styles fixed

--- src/easymed/patient/PatientReserveVisitPopup.js
+++ src/easymed/patient/PatientReserveVisitPopup.js
@@ -2,22 +2,24 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import Typography from '@mui/material/Typography';
-import { Box, MenuItem, TextField } from '@mui/material';
+import { Box, MenuItem, TextField, Alert } from '@mui/material';
 import SelectDateField from './reserve-visit-components/SelectDateField';
 import ReserveVisitDateTile from './reserve-visit-components/ReserveVisitDateTile';
 import ReserveVisitDoctorTile from './reserve-visit-components/ReserveVisitDoctorTile';
 import ReserveVisitTitle from './reserve-visit-components/ReserveVisitTitle';
-
+import { useFetchUrl } from './reserve-visit-components/useFetchUrl';
+import CheckIcon from '@mui/icons-material/Check';
 
 const specializationList = ['Orthopedic', 'Kardiologist','Radiologist'] 
 const doctorsList = ['Maciej Bulwa', 'Adrian Kunsz','Barbara Telejko'] 
 
-function PatientReserveVisitPopup({children}) {
+function PatientReserveVisitPopup() {
     const [open, setOpen] = React.useState(false);
     const [selectedDate, setSelectedDate] = React.useState(null);
     const [reservationOption, setReservationOption] = React.useState("default");
-    const [selectedSpecialization, setSelectedSpecialization] = React.useState(null);
-    const [selectedDoctor, setSelectedDoctor] = React.useState(null);
+    const [selectedSpecialization, setSelectedSpecialization] = React.useState('');
+    const [selectedDoctor, setSelectedDoctor] = React.useState("");
+    const isAllDataComplete = (selectedDate && selectedSpecialization !== '' && selectedDoctor !== '');
   
     const handleOpen = () => {
       setOpen(true);
@@ -25,8 +27,20 @@ function PatientReserveVisitPopup({children}) {
 
     const handleClose = () => {
       setOpen(false); 
-      setTimeout(() => {setReservationOption("default"); setSelectedDate(null); setSelectedSpecialization(null)}, 400);   
+      setTimeout(() => {
+        setReservationOption("default"); 
+        setSelectedDate(null); 
+        setSelectedSpecialization('');
+        setSelectedDoctor('');}
+        , 400);   
     };
+
+    const handleSubmitVisit = () => {
+      console.log(selectedDate);
+      console.log(selectedSpecialization);
+      console.log(selectedDoctor);
+      handleClose();
+    }
   
     return (
       <>
@@ -49,6 +63,7 @@ function PatientReserveVisitPopup({children}) {
               ? 
               <Box
                 sx={{
+                  width: "100%",
                   mt: 4,
                   mb: 4,
                   display: "flex",
@@ -68,6 +83,7 @@ function PatientReserveVisitPopup({children}) {
                   (reservationOption === "date" ? 
                   <Box
                   sx={{
+                    width: "80%",
                     mt: 4,
                     mb: 4,
                     display: "flex",
@@ -77,27 +93,78 @@ function PatientReserveVisitPopup({children}) {
                   }}
                 >
                   <SelectDateField currentDate={selectedDate} onChange={setSelectedDate}/>
-                  <TextField label="Select specialization" 
+                  {selectedDate
+                        ? 
+                        <TextField label="Select specialization" 
                           value={selectedSpecialization} 
                           onChange={(event) => {setSelectedSpecialization(event.target.value)}} 
                           select
                           fullWidth
                           >
-                     {specializationList.map((spec, idx) => <MenuItem value={`S${idx}`}>{spec}</MenuItem>)}
-                  </TextField>
-                  <TextField label="Select doctor" 
+                     {specializationList.map((spec, idx) => <MenuItem key={idx} value={`S${idx}`}>{spec}</MenuItem>)}
+                      </TextField> 
+                        : 
+                        null}
+
+                  {(selectedDate && selectedSpecialization !== '')
+                    ?
+                    <TextField label="Select doctor" 
                           value={selectedDoctor} 
                           onChange={(event) => {setSelectedDoctor(event.target.value)}} 
                           select
                           fullWidth
                           >
-                     {doctorsList.map((doc, idx) => <MenuItem value={`S${idx}`}>{doc}</MenuItem>)}
-                  </TextField>
+                     {doctorsList.map((doc, idx) => <MenuItem key={idx} value={`S${idx}`}>{doc}</MenuItem>)}
+                  </TextField> 
+                    :
+                    null
+                  }
 
+                  {isAllDataComplete ? <Button variant="contained" color={"primary"} onClick={handleSubmitVisit}>CONFIRM VISIT</Button> : null}
+                  
                 </Box> 
-                : <Typography varaint="h3" color={"primary.main"} fontWeight={"bold"}>
-                   DOCTOR
-                  </Typography>
+                : <Box
+                    sx={{
+                      width: "80%",
+                      mt: 4,
+                      mb: 4,
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      gap: 5,
+                    }}>
+                
+                <TextField label="Select specialization" 
+                        value={selectedSpecialization} 
+                        onChange={(event) => {setSelectedSpecialization(event.target.value)}} 
+                        select
+                        fullWidth
+                        >
+                   {specializationList.map((spec, idx) => <MenuItem key={idx} value={`S${idx}`}>{spec}</MenuItem>)}
+                </TextField> 
+                {selectedSpecialization !== ''
+                      ? 
+                      <TextField label="Select doctor" 
+                        value={selectedDoctor} 
+                        onChange={(event) => {setSelectedDoctor(event.target.value)}} 
+                        select
+                        fullWidth
+                        >
+                   {doctorsList.map((doc, idx) => <MenuItem key={idx} value={`S${idx}`}>{doc}</MenuItem>)}
+                </TextField> 
+                      : 
+                      null}
+
+                {(selectedDoctor !== '' && selectedSpecialization !== '')
+                  ?
+                  <SelectDateField currentDate={selectedDate} onChange={setSelectedDate}/>
+                  :
+                  null
+                }
+
+                {isAllDataComplete ? <Button variant="contained" color={"primary"} onClick={handleSubmitVisit}>CONFIRM VISIT</Button> : null}
+                
+              </Box>
                 )}
           </Box>
           </Dialog>
--- src/easymed/patient/reserve-visit-components/ReserveVisitTitle.js
+++ src/easymed/patient/reserve-visit-components/ReserveVisitTitle.js
@@ -5,7 +5,7 @@ import { Typography, Divider } from '@mui/material';
 export default function ReserveVisitTitle() {
     return <>
             <Typography variant="h5" color={"primary.main"} sx={{ my: 2 }} fontWeight={"bold"}>Reserve your visit today!</Typography>
-            <Divider sx={{ width: "90%" }}/>
+            <Divider sx={{ width: "80%" }}/>
             </> 
 
 }
