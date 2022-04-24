5c0e25db59833636d3a8257df0da7a0c1d9be368
Adrian <a.milewski@onet.eu>
2022-04-24 22:09:54 +0200

    Parametrize doctor dashboard

--- src/easymed/doctor/dashboard/DailyTile.js
+++ src/easymed/doctor/dashboard/DailyTile.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import {Paper, Typography, useMediaQuery} from "@mui/material";
+import React from "react";
+import { Paper, Typography, useMediaQuery } from "@mui/material";
 import Box from "@mui/material/Box";
-import {styled} from "@mui/material/styles";
-import {useTheme} from "@emotion/react";
+import { styled } from "@mui/material/styles";
+import { useTheme } from "@emotion/react";
 
 const TileIcon = styled("img")(({ theme }) => ({
   padding: theme.spacing(1),
-  border: '2px solid green',
-  backgroundColor: 'lightGreen',
-  borderRadius: '50%'
+  borderWidth: "2px",
+  borderStyle: "solid",
+  borderRadius: "50%",
+  borderColor: "grey",
+  backgroundColor: "lightGrey",
+  boxShadow: "8px 8px 24px -12px rgba(66, 68, 90, 0.8)",
 }));
 
-const DailyTile = ({name, value, iconImgUrl, bgColor, ...props}) => {
-  const theme = useTheme()
-  const matchesMobile = useMediaQuery(theme.breakpoints.down("sm"))
+const DailyTile = ({ name, value, iconImgUrl, bgColor, ...props }) => {
+  const theme = useTheme();
+  const matchesMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
-    <Paper sx={{px: 2, py: 3, display: 'flex', alignItems: 'center', gap: 2, flex: matchesMobile ? "1": "0 1 auto"}}>
-      <TileIcon src={"/images/doctor/dashboard/calendar-icon.png"} alt={""} />
+    <Paper
+      sx={{
+        px: 2,
+        py: 3,
+        display: "flex",
+        alignItems: "center",
+        gap: 2,
+        flex: matchesMobile ? "1" : "0 1 auto",
+      }}
+    >
+      <TileIcon
+        src={iconImgUrl}
+        alt={""}
+        sx={{
+          borderColor: `rgba(${bgColor}, 1.0)`,
+          backgroundColor: `rgba(${bgColor}, 0.5)`,
+        }}
+      />
       <Box>
-        <Typography fontWeight={"bold"}>7</Typography>
-        <Typography sx={{color: "text.secondary"}} variant={"body2"}>Remaining visits</Typography>
+        <Typography variant={"h5"} fontWeight={"bold"}>
+          {value}
+        </Typography>
+        <Typography sx={{ color: "text.secondary" }} variant={"body2"}>
+          {name}
+        </Typography>
       </Box>
     </Paper>
   );
--- src/easymed/doctor/dashboard/DoctorDashboard.js
+++ src/easymed/doctor/dashboard/DoctorDashboard.js
@@ -1,16 +1,48 @@
 import React from "react";
-import {Box, Typography} from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import DailyTile from "./DailyTile";
 
 const DoctorDashboard = () => {
   return (
-    <Box sx={{py: 2, px: 5}}>
-      <Typography fontWeight={"bold"} sx={{color: 'text.secondary'}} variant={"h6"}>Daily info</Typography>
-      <Box display={"flex"} justifyContent={"space-between"} gap={2} paddingY={2} flexWrap={"wrap"}>
-        <DailyTile />
-        <DailyTile />
-        <DailyTile />
-        <DailyTile />
+    <Box sx={{ py: 2, px: 5 }}>
+      <Typography
+        fontWeight={"bold"}
+        sx={{ color: "text.secondary" }}
+        variant={"h6"}
+      >
+        Daily info
+      </Typography>
+      <Box
+        display={"flex"}
+        justifyContent={"space-between"}
+        gap={2}
+        paddingY={2}
+        flexWrap={"wrap"}
+      >
+        <DailyTile
+          name={"Remaining visits"}
+          value={7}
+          bgColor={"0, 200, 0"}
+          iconImgUrl={"/images/doctor/dashboard/calendar-icon.png"}
+        />
+        <DailyTile
+          name={"Remaining visits"}
+          value={7}
+          bgColor={"0, 200, 0"}
+          iconImgUrl={"/images/doctor/dashboard/calendar-icon.png"}
+        />
+        <DailyTile
+          name={"Remaining visits"}
+          value={7}
+          bgColor={"0, 200, 0"}
+          iconImgUrl={"/images/doctor/dashboard/calendar-icon.png"}
+        />
+        <DailyTile
+          name={"Remaining visits"}
+          value={7}
+          bgColor={"0, 200, 0"}
+          iconImgUrl={"/images/doctor/dashboard/calendar-icon.png"}
+        />
       </Box>
     </Box>
   );
