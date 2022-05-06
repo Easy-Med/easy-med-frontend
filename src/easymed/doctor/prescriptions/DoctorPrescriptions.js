a29f612a7e1a726e92c6780459afa8a3610ebf94
Adrian <a.milewski@onet.eu>
2022-05-06 10:42:59 +0200

    Add prescriptions v2

--- src/easymed/doctor/prescriptions/DoctorPrescriptions.js
+++ src/easymed/doctor/prescriptions/DoctorPrescriptions.js
@@ -1,7 +1,26 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
+import IssuePrescriptionForm from "./IssuePrescriptionForm";
 
 const DoctorPrescriptions = () => {
-  return <div>Prescriptions</div>;
+  return (
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      alignItems={"flex-start"}
+      p={2}
+      gap={2}
+    >
+      <Typography variant={"h4"} color={"text.secondary"}>
+        Issue a prescription
+      </Typography>
+      <IssuePrescriptionForm sx={{ width: "100%" }} />
+      <Typography variant={"h4"} color={"text.secondary"}>
+        Issued prescriptions
+      </Typography>
+    </Box>
+  );
 };
 
 export default DoctorPrescriptions;
---
+++ src/easymed/doctor/prescriptions/IssuePrescriptionForm.js
@@ -0,0 +1,166 @@
+import React, { useState } from "react";
+import {
+  Autocomplete,
+  Button,
+  CircularProgress,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+
+const initFormData = {
+  patient: null,
+  medicines: [],
+};
+
+const initMedicine = {
+  name: "",
+  capacity: "",
+};
+
+const optionss = [
+  { name: "Patryk Grzechuta", id: 2 },
+  { name: "Marek Dąb", id: 3 },
+];
+
+const options = [];
+
+const IssuePrescriptionForm = ({ sx = [] }) => {
+  const [formData, setFormData] = useState(initFormData);
+  const [medicine, setMedicine] = useState(initMedicine);
+  const [patientInputValue, setPatientInputValue] = useState("");
+  const [loadingPatients, setLoadingPatients] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const handleMedicineChange = (e) => {
+    setMedicine({ ...medicine, [e.target.name]: e.target.value });
+  };
+
+  const handleAddMedicine = (e) => {
+    e.preventDefault();
+    setFormData({ ...formData, medicines: [...formData.medicines, medicine] });
+    setMedicine(initMedicine);
+  };
+
+  const handleSubmitPrescription = (e) => {
+    e.preventDefault();
+    console.log("Send data", formData);
+  };
+
+  const resetFormData = () => {
+    setFormData(initFormData);
+    setMedicine(initMedicine);
+  };
+
+  return (
+    <Paper
+      elevation={2}
+      sx={[
+        {
+          display: "flex",
+          flexDirection: "column",
+        },
+        ...(Array.isArray(sx) ? sx : [sx]),
+      ]}
+      component={"form"}
+      onSubmit={handleSubmitPrescription}
+    >
+      <Box display={"flex"} width={"100%"} p={2}>
+        <Box display={"flex"} flexDirection={"column"} gap={1} width={"48%"}>
+          <Autocomplete
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                variant={"standard"}
+                label={"Patient"}
+                required
+                InputProps={{
+                  ...params.InputProps,
+                  endAdornment: (
+                    <React.Fragment>
+                      {loadingPatients ? (
+                        <CircularProgress color="inherit" size={20} />
+                      ) : null}
+                      {params.InputProps.endAdornment}
+                    </React.Fragment>
+                  ),
+                }}
+              />
+            )}
+            options={options}
+            open={open}
+            onOpen={() => {
+              setLoadingPatients(true);
+              setOpen(true);
+            }}
+            onClose={() => {
+              setLoadingPatients(false);
+              setOpen(false);
+            }}
+            isOptionEqualToValue={(option, value) => option.name === value.name}
+            getOptionLabel={(option) => option.name}
+            loading={loadingPatients}
+            value={formData.patient}
+            onChange={(e, newValue) =>
+              setFormData({ ...formData, patient: newValue })
+            }
+            inputValue={patientInputValue}
+            onInputChange={(event, newInputValue) =>
+              setPatientInputValue(newInputValue)
+            }
+          />
+          <TextField
+            variant={"standard"}
+            name={"name"}
+            label={"Medicine Name"}
+            value={medicine.name}
+            onChange={handleMedicineChange}
+          />
+          <TextField
+            variant={"standard"}
+            name={"capacity"}
+            label={"Medicine Capacity"}
+            value={medicine.capacity}
+            onChange={handleMedicineChange}
+          />
+          <Button
+            onClick={handleAddMedicine}
+            variant={"outlined"}
+            sx={{ alignSelf: "flex-start", mt: 2 }}
+          >
+            Add medicine
+          </Button>
+        </Box>
+        <Divider orientation={"vertical"} sx={{ mx: 2 }} flexItem />
+        <Box display={"flex"} flexDirection={"column"}>
+          <Typography variant={"h5"}>Prescribed medicines</Typography>
+          <ul>
+            {formData.medicines.map((medicine, index) => (
+              <li key={index}>
+                {medicine.name} {medicine.capacity}
+              </li>
+            ))}
+          </ul>
+        </Box>
+      </Box>
+      <Box
+        bgcolor={"grey.200"}
+        p={2}
+        display={"flex"}
+        justifyContent={"flex-end"}
+        gap={2}
+      >
+        <Button onClick={resetFormData} variant={"contained"} color={"error"}>
+          Reset
+        </Button>
+        <Button type={"submit"} variant={"contained"}>
+          Submit
+        </Button>
+      </Box>
+    </Paper>
+  );
+};
+
+export default IssuePrescriptionForm;
