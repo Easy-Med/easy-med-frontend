423671238ccd150f51749740b630bf9f211bfff9
malinowa <64161347+malinowa@users.noreply.github.com>
2022-05-14 21:11:45 +0200

    Fixed conflict with develop

---
+++ src/easymed/patient/reserve-visit-components/ReserveVisitDoctorTile.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReserveVisitOptionTile from "./ReserveVisitOptionTile";
+import { motion } from "framer-motion";
+
+const ReserveVisitDateTile = () => {
+  return (
+    <motion.div
+      whileInView={{ y: [100, 0], opacity: [0, 1] }}
+      transition={{ duration: 0.5, delay: 0.05 }}
+      style={{ opacity: 0 }}
+    >
+      <ReserveVisitOptionTile
+        title={"Choose doctor"}
+        imgUrl={"/images/doctor/doctor-login-screen.png"}
+        imgAlt={"doctor-image-tile"}
+        redirectUrl={""}
+      />
+    </motion.div>
+  );
+};
+
+export default ReserveVisitDateTile;
