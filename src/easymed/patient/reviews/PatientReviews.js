515e91d2f4b7af6a8cd734fa99df18e7818b1a52
Adrian <a.milewski@onet.eu>
2022-05-15 13:52:32 +0200

    Add reviews skeleton to patient

--- src/easymed/patient/reviews/PatientReviews.js
+++ src/easymed/patient/reviews/PatientReviews.js
@@ -1,11 +1,75 @@
 import React from "react";
 import Box from "@mui/material/Box";
+import PageBox from "../../generic/PageBox";
+import { Typography } from "@mui/material";
+import PostReviewCard from "./PostReviewCard";
+import ReviewCard from "../../generic/reviews/ReviewCard";
+import useAuth from "../../../app/auth/UseAuth";
+import { useQuery } from "react-query";
+import ReviewsService from "../../../app/api/ReviewsService";
+
+const postReviewInfos = [
+  {
+    doctorName: "",
+    doctorSpecialization: "Alergolog",
+    dateOfVisit: "04.03.2022",
+  },
+  {
+    doctorName: "",
+    doctorSpecialization: "Alergolog",
+    dateOfVisit: "04.03.2022",
+  },
+];
 
 const PatientReviews = () => {
-  return (
-    <Box p={2}>
+  const auth = useAuth();
+  const { role, id } = auth.authData;
 
-    </Box>
+  const reviewQuery = useQuery(`${role}Reviews`, () =>
+    ReviewsService.getReviewsFor(role, id)
+  );
+
+  if (reviewQuery.isError) {
+    return (
+      <Typography p={2} variant={"h5"}>
+        Can't load reviews :/. Try again later!
+      </Typography>
+    );
+  }
+
+  if (reviewQuery.isLoading) {
+    return (
+      <PageBox bgcolor={"grey.900"}>
+        <Typography variant={"h5"} mb={4}>
+          Loading...
+        </Typography>
+      </PageBox>
+    );
+  }
+
+  return (
+    <PageBox>
+      <Typography variant={"h5"} color={"info"}>
+        Post Review
+      </Typography>
+      <Box sx={{ mt: 1, mb: 3 }}>
+        {postReviewInfos.map((info, idx) => (
+          <PostReviewCard key={idx} postReviewInfo={info} />
+        ))}
+      </Box>
+      <Typography variant={"h5"} color={"info"}>
+        Received reviews
+      </Typography>
+      <Box sx={{ mt: 1, mb: 3 }}>
+        {reviewQuery.data.length !== 0 ? (
+          reviewQuery.data.map((review) => <ReviewCard review={review} />)
+        ) : (
+          <Typography variant={"h6"}>
+            Write your first review today! ✨
+          </Typography>
+        )}
+      </Box>
+    </PageBox>
   );
 };
 
