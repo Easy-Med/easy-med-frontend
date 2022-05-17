950dcc0b58fcd2278e83c496dab9be8b2187b404
Adrian <a.milewski@onet.eu>
2022-05-17 19:11:13 +0200

    Correct formatting

--- src/easymed/patient/reserveNewVisit/ReserveVisitTitle.js
+++ src/easymed/patient/reserveNewVisit/ReserveVisitTitle.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import { Typography, Divider } from '@mui/material';
-
+import React from "react";
+import { Divider, Typography } from "@mui/material";
 
 export default function ReserveVisitTitle() {
-    return <>
-            <Typography variant="h5" color={"primary.main"} sx={{ my: 2 }} fontWeight={"bold"}>Reserve your visit today!</Typography>
-            <Divider sx={{ width: "80%" }}/>
-            </> 
-
+  return (
+    <>
+      <Typography
+        variant="h5"
+        color={"primary.main"}
+        sx={{ my: 2 }}
+        fontWeight={"bold"}
+      >
+        Reserve your visit today!
+      </Typography>
+      <Divider sx={{ width: "80%" }} />
+    </>
+  );
 }
