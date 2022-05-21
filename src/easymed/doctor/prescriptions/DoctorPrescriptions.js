cf7fe7dc6562a150db68a6186e77a786f9ca17dc
Adrian <a.milewski@onet.eu>
2022-05-21 13:59:03 +0200

    Add prescriptions backend

--- src/easymed/doctor/prescriptions/DoctorPrescriptions.js
+++ src/easymed/doctor/prescriptions/DoctorPrescriptions.js
@@ -1,56 +1,20 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import { Typography } from "@mui/material";
+import { Skeleton, Typography } from "@mui/material";
 import IssuePrescriptionForm from "./IssuePrescriptionForm";
 import PrescriptionCard from "../../generic/prescriptions/PrescriptionCard";
 import PageBox from "../../generic/PageBox";
-
-const prescriptions = [
-  {
-    patientName: "Alex Mandel",
-    dateOfIssue: "02.04.2022",
-    medicines: [
-      {
-        name: "Dymista",
-        capacity: "20mg",
-      },
-      {
-        name: "Altacet",
-        capacity: "100ml",
-      },
-    ],
-  },
-  {
-    patientName: "Alex Mandel",
-    dateOfIssue: "02.04.2022",
-    medicines: [
-      {
-        name: "Dymista",
-        capacity: "20mg",
-      },
-      {
-        name: "Altacet",
-        capacity: "100ml",
-      },
-    ],
-  },
-  {
-    patientName: "Alex Mandel",
-    dateOfIssue: "02.04.2022",
-    medicines: [
-      {
-        name: "Dymista",
-        capacity: "20mg",
-      },
-      {
-        name: "Altacet",
-        capacity: "100ml",
-      },
-    ],
-  },
-];
+import { useQuery } from "react-query";
+import PrescriptionsService from "../../../app/api/PrescriptionsService";
+import useAuth from "../../../app/auth/UseAuth";
 
 const DoctorPrescriptions = () => {
+  const auth = useAuth();
+  const { id, role } = auth.authData;
+  const prescriptionsQuery = useQuery("doctorPrescriptions", () =>
+    PrescriptionsService.getPrescriptionsFor(role, id)
+  );
+
   return (
     <PageBox sx={{ gap: 2 }}>
       <Typography variant={"h4"} color={"text.secondary"}>
@@ -60,17 +24,31 @@ const DoctorPrescriptions = () => {
       <Typography variant={"h4"} color={"text.secondary"}>
         Issued prescriptions
       </Typography>
-      <Box display={"flex"} gap={2} flexWrap={"wrap"}>
-        {prescriptions.map((prescription) => (
-          <PrescriptionCard
-            personOnPx={prescription.patientName}
-            dateOfIssue={prescription.dateOfIssue}
-            listOfMedicines={prescription.medicines.map(
-              (medicine) => medicine.name + " " + medicine.capacity
-            )}
-          />
-        ))}
-      </Box>
+      {prescriptionsQuery.isLoading && (
+        <Skeleton width={300} height={270} variant={"rectangular"} />
+      )}
+      {prescriptionsQuery.isError && (
+        <Typography variant={"h6"}>
+          Can't load prescriptions :/. Try again later!
+        </Typography>
+      )}
+      {prescriptionsQuery.isSuccess &&
+        prescriptionsQuery.data?.length === 0 && (
+          <Typography>No issued prescriptions :o. Go and issue one!</Typography>
+        )}
+      {prescriptionsQuery.isSuccess && prescriptionsQuery.data?.length !== 0 && (
+        <Box display={"flex"} gap={2} flexWrap={"wrap"}>
+          {prescriptionsQuery.data.map((prescription) => (
+            <PrescriptionCard
+              personOnPx={prescription.patientName}
+              dateOfIssue={prescription.dateOfIssue}
+              listOfMedicines={prescription.medicines.map(
+                (medicine) => medicine.name + " " + medicine.capacity
+              )}
+            />
+          ))}
+        </Box>
+      )}
     </PageBox>
   );
 };
