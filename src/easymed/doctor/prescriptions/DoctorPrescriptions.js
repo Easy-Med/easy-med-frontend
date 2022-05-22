369e1de92b9c0d5a3646303a87e5565312be5173
Adrian Milewski <56368449+milewsa3@users.noreply.github.com>
2022-05-22 13:08:45 +0200

    EAS-9 Utworzenie recepty doktor && EAS-10 Wyświetlenie recepty (pacjent) (#25)
    * Add issue a prescription function
    
    * Add prescriptions backend
    
    * Add prescriptions to patient

--- src/easymed/doctor/prescriptions/DoctorPrescriptions.js
+++ src/easymed/doctor/prescriptions/DoctorPrescriptions.js
@@ -1,54 +1,8 @@
 import React from "react";
-import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
 import IssuePrescriptionForm from "./IssuePrescriptionForm";
-import PrescriptionCard from "../../generic/prescriptions/PrescriptionCard";
 import PageBox from "../../generic/PageBox";
-
-const prescriptions = [
-  {
-    patientName: "Alex Mandel",
-    dateOfIssue: "02.04.2022",
-    medicines: [
-      {
-        name: "Dymista",
-        capacity: "20mg",
-      },
-      {
-        name: "Altacet",
-        capacity: "100ml",
-      },
-    ],
-  },
-  {
-    patientName: "Alex Mandel",
-    dateOfIssue: "02.04.2022",
-    medicines: [
-      {
-        name: "Dymista",
-        capacity: "20mg",
-      },
-      {
-        name: "Altacet",
-        capacity: "100ml",
-      },
-    ],
-  },
-  {
-    patientName: "Alex Mandel",
-    dateOfIssue: "02.04.2022",
-    medicines: [
-      {
-        name: "Dymista",
-        capacity: "20mg",
-      },
-      {
-        name: "Altacet",
-        capacity: "100ml",
-      },
-    ],
-  },
-];
+import PrescriptionsList from "../../generic/prescriptions/PrescriptionsList";
 
 const DoctorPrescriptions = () => {
   return (
@@ -60,17 +14,7 @@ const DoctorPrescriptions = () => {
       <Typography variant={"h4"} color={"text.secondary"}>
         Issued prescriptions
       </Typography>
-      <Box display={"flex"} gap={2} flexWrap={"wrap"}>
-        {prescriptions.map((prescription) => (
-          <PrescriptionCard
-            personOnPx={prescription.patientName}
-            dateOfIssue={prescription.dateOfIssue}
-            listOfMedicines={prescription.medicines.map(
-              (medicine) => medicine.name + " " + medicine.capacity
-            )}
-          />
-        ))}
-      </Box>
+      <PrescriptionsList />
     </PageBox>
   );
 };
---
+++ src/easymed/generic/prescriptions/PrescriptionsList.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Skeleton, Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import PrescriptionCard from "./PrescriptionCard";
+import useAuth from "../../../app/auth/UseAuth";
+import { useQuery } from "react-query";
+import PrescriptionsService from "../../../app/api/PrescriptionsService";
+
+const PrescriptionsList = () => {
+  const auth = useAuth();
+  const { id, role } = auth.authData;
+  const prescriptionsQuery = useQuery(`${role}Prescriptions`, () =>
+    PrescriptionsService.getPrescriptionsFor(role, id)
+  );
+
+  return (
+    <>
+      {prescriptionsQuery.isLoading && (
+        <Skeleton width={300} height={270} variant={"rectangular"} />
+      )}
+      {prescriptionsQuery.isError && (
+        <Typography variant={"h6"}>
+          Can't load prescriptions :/. Try again later!
+        </Typography>
+      )}
+      {prescriptionsQuery.isSuccess &&
+        prescriptionsQuery.data?.length === 0 && (
+          <Typography>No issued prescriptions :o. Go and issue one!</Typography>
+        )}
+      {prescriptionsQuery.isSuccess && prescriptionsQuery.data?.length !== 0 && (
+        <Box display={"flex"} gap={2} flexWrap={"wrap"}>
+          {prescriptionsQuery.data.map((prescription) => (
+            <PrescriptionCard
+              isForPatient
+              personOnPx={
+                role === "doctor"
+                  ? prescription.patientName
+                  : prescription.doctorName
+              }
+              dateOfIssue={prescription.dateOfIssue}
+              listOfMedicines={prescription.medicines.map(
+                (medicine) => medicine.name + " " + medicine.capacity
+              )}
+            />
+          ))}
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default PrescriptionsList;
