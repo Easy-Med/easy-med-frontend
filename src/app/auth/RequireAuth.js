2318720512c7feabdd68a3e3c19468ec6d1e6417
Adrian <a.milewski@onet.eu>
2022-05-24 14:42:28 +0200

    Add token invalidation

--- src/app/auth/RequireAuth.js
+++ src/app/auth/RequireAuth.js
@@ -1,20 +1,28 @@
 import React from "react";
 import useAuth from "./UseAuth";
 import { Navigate, useLocation } from "react-router-dom";
+import jwtDecode from "jwt-decode";
 
 const RequireAuth = ({ children, role }) => {
   const auth = useAuth();
   const location = useLocation();
 
+  const isTokenExpired = () => {
+    const { accessToken } = auth.authData;
+    const decodedToken = jwtDecode(accessToken);
+
+    return decodedToken.exp * 1000 < new Date().getTime();
+  };
+
+  const isWrongRole = () => {
+    return auth.authData.role.toLowerCase() !== role;
+  };
+
   if (!auth.authData) {
-    // Redirect them to the signIn page, but save the current location they were
-    // trying to go to when they were redirected. This allows us to send them
-    // along to that page after they signIn, which is a nicer user experience
-    // than dropping them off on the home page.
     return <Navigate to="/signIn" state={{ from: location }} replace />;
   }
 
-  if (auth.authData.role.toLowerCase() !== role) {
+  if (isTokenExpired() || isWrongRole()) {
     auth.signOut();
     return <Navigate to="/signIn" state={{ from: location }} replace />;
   }
