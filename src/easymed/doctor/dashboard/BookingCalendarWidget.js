e9b2a5b81eb9910adcfea5a182e808adde9301bd
Adrian <a.milewski@onet.eu>
2022-05-30 20:02:45 +0200

    Fix date time

--- src/easymed/doctor/dashboard/BookingCalendarWidget.js
+++ src/easymed/doctor/dashboard/BookingCalendarWidget.js
@@ -1,24 +1,24 @@
 import React from "react";
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
-import {Paper, useMediaQuery} from "@mui/material";
+import { Paper, useMediaQuery } from "@mui/material";
 import { useQuery } from "react-query";
 import ReservedVisitsService from "../../../app/api/ReservedVisitsService";
 import CalendarWidgetMapper from "./CalendarWidgetMapper";
 import useAuth from "../../../app/auth/UseAuth";
-import {useTheme} from "@emotion/react";
+import { useTheme } from "@emotion/react";
 
 const eventTimeFormat = {
   hour: "2-digit",
   minute: "2-digit",
-  meridiem: false,
+  hour12: false,
 };
 
 const BookingCalendarWidget = () => {
   const auth = useAuth();
   const { id } = auth.authData;
-  const theme = useTheme()
-  const matchesDesktop = useMediaQuery(theme.breakpoints.up('md'))
+  const theme = useTheme();
+  const matchesDesktop = useMediaQuery(theme.breakpoints.up("md"));
 
   const reservedVisitsQuery = useQuery(`doctorVisits`, () =>
     ReservedVisitsService.getReservedVisitsFor("doctor", id, {})
@@ -29,7 +29,7 @@ const BookingCalendarWidget = () => {
       <FullCalendar
         plugins={[dayGridPlugin]}
         initialView="dayGridMonth"
-        height={'100%'}
+        height={"100%"}
         contentHeight={matchesDesktop ? "" : "auto"}
         events={
           reservedVisitsQuery.isSuccess
